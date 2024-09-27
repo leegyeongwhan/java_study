3a968365f91ebca8c179c8a8c3d65864a29101c9
gylee <leekhy02@naver.com>
2024-09-27 23:46:33 +0900

    feat : builder 가변인자 추가

--- effective_java/src/main/java/effective/ch2/Person.java
+++ effective_java/src/main/java/effective/ch2/Person.java
@@ -1,4 +1,40 @@
 package effective.ch2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+    private String name;
+    private int birtYear;
+    private List<String> kids;
+
+    public Person(String name, int birtYear) {
+        this.name = name;
+        this.birtYear = birtYear;
+        this.kids = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirtYear() {
+        return birtYear;
+    }
+
+    public void setBirtYear(int birtYear) {
+        this.birtYear = birtYear;
+    }
+
+    public List<String> getKids() {
+        return kids;
+    }
+
+    public void setKids(List<String> kids) {
+        this.kids = kids;
+    }
 }
